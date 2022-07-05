78e2eb57f161a96b55ac14bc96477b003e6e7c01
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-05 18:17:44 +0530

    Update CheckBattery.h

--- CheckBattery.h
+++ CheckBattery.h
@@ -4,10 +4,5 @@
 #define SOC_MAX				80
 #define CHARGERATE_LIMIT	0.8
 
-#define BATTERY_PARAMATER_WITHIN_RANGE(param, min, max)		((param > min) && (param < max))
-#define CHARGERATE_OUTOF_RANGE(param, limit)				(param > limit)
-
-int 	validateTemperature	(float temperature);
-int 	validateSOC			(float soc);
-int 	validateChargeRate	(float chargeRate);
-int 	batteryIsOk			(float temperature, float soc, float chargeRate);
+#define BATTERY_CHECK(param, min, max)		((param < min) || (param > max))
+#define CHARGERATE_CHECK(param, limit)		(param > limit)
