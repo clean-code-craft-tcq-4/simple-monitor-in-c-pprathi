10c07909c65a375947c37a379020528fac9c5ca0
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-05 20:05:23 +0530

    Update CheckBattery.c

--- CheckBattery.c
+++ CheckBattery.c
@@ -2,15 +2,16 @@
 #include <assert.h>
 #include "CheckBattery.h"
 
-
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-	return (!BATTERY_CHECK(temperature, TEMP_MIN, TEMP_MAX) && !BATTERY_CHECK(soc, SOC_MIN, SOC_MAX) && !CHARGERATE_CHECK(chargeRate, CHARGERATE_LIMIT));
+	return (!BATTERY_PARAM_NOTINRANGE(temperature, TEMP_MIN, TEMP_MAX) && !BATTERY_PARAM_NOTINRANGE(soc, SOC_MIN, SOC_MAX) && 
+		!CHARGERATE_NOTINRANGE(chargeRate, CHARGERATE_LIMIT));
 }
 
 int main(void) {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(46, 21, 0.6));
-  assert(!batteryIsOk(44, 19, 0.1));
-  assert(!batteryIsOk(1, 79, 0.9));
+  assert(batteryIsOk(25, 70, 0.7)); 	//All params in Range
+  assert(!batteryIsOk(50, 85, 0)); 	//Temperature, SOC out of range
+  assert(!batteryIsOk(46, 21, 0.6));	//Temperture out of range
+  assert(!batteryIsOk(44, 19, 0.1));	//SOC out of range
+  assert(!batteryIsOk(1, 79, 0.9));	//ChargeRate out of range
+  assert(!batteryIsOk(-1, 80, 0.8));	//Temperature out of range
 }
