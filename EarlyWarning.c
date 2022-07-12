4401b2f4fbd7c1bac24b96e4ecafc23571a92eb4
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-13 01:25:27 +0530

    Update EarlyWarning.c

--- EarlyWarning.c
+++ EarlyWarning.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "BatteryChecker.h"
 
-extern tst_BatteryStatus batteryStatus_st;
-
 int CheckForWarning(ten_BatteryParameter parameter, float value, float min, float max){
 	int warningSts = 0;
 	if((value > min) && (value <= min + max*TOLERANCE)){
@@ -12,11 +10,6 @@ int CheckForWarning(ten_BatteryParameter parameter, float value, float min, floa
 	else if((value >= max - max*TOLERANCE) && (value <= max)){
 		UpdateForHighLimitWarning(parameter);
 		warningSts = 1;
-	}
-	else{
-		batteryStatus_st.TempStatus = TEMP_NORMAL;
-		batteryStatus_st.SOCStatus = SOC_NORMAL;
-		batteryStatus_st.ChargeRateStatus = CHARGERATE_NORMAL;
 	}
 	return warningSts;
 }
