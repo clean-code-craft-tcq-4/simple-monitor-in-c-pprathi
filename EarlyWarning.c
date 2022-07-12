d5aa59286355c1205ed1bd7029ce5cf1b2aa7057
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-13 01:20:26 +0530

    Update EarlyWarning.c

--- EarlyWarning.c
+++ EarlyWarning.c
@@ -3,36 +3,6 @@
 
 extern tst_BatteryStatus batteryStatus_st;
 
-void UpdateForLowLimitWarning(ten_BatteryParameter parameter){
-	if(parameter == TEMPERATURE){
-		batteryStatus_st.TempStatus = LOW_TEMP_WARNING;
-		printf("\nLow Temperature warning");
-	}
-	else if(parameter == SOC){
-		batteryStatus_st.SOCStatus = LOW_SOC_WARNING;
-		printf("\nLow SOC warning");
-	}
-	else if(parameter == CHARGERATE){
-		batteryStatus_st.ChargeRateStatus = LOW_CHARGERATE_WARNING;
-		printf("\nLow ChargeRate warning");
-	}
-}
-
-void UpdateForHighLimitWarning(ten_BatteryParameter parameter){
-	if(parameter == TEMPERATURE){
-		batteryStatus_st.TempStatus = HIGH_TEMP_WARNING;
-		printf("\nHigh Temperature warning");
-	}
-	else if(parameter == SOC){
-		batteryStatus_st.SOCStatus = HIGH_SOC_WARNING;
-		printf("\nHigh SOC warning");
-	}
-	else if(parameter == CHARGERATE){
-		batteryStatus_st.ChargeRateStatus = HIGH_CHARGERATE_WARNING;
-		printf("\nHigh ChargeRate warning");
-	}
-}
-
 int CheckForWarning(ten_BatteryParameter parameter, float value, float min, float max){
 	int warningSts = 0;
 	if((value > min) && (value <= min + max*TOLERANCE)){
