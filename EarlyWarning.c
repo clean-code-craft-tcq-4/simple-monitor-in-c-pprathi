6bf7b15eb54d8a5dd71faa44f5b841b27a6a9938
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-13 01:30:35 +0530

    Update EarlyWarning.c

--- EarlyWarning.c
+++ EarlyWarning.c
@@ -2,12 +2,14 @@
 #include "BatteryChecker.h"
 
 int CheckForWarning(ten_BatteryParameter parameter, float value, float min, float max){
-	int warningSts = 0;
-	if((value > min) && (value <= min + max*TOLERANCE)){
+	int warningSts = 0, lowLimitCheck = 0, highLimitCheck = 0;
+	lowLimitCheck = (value > min) && (value <= min + max*TOLERANCE);
+	highLimitCheck = (value >= max - max*TOLERANCE) && (value <= max);
+	if(lowLimitCheck){
 		UpdateForLowLimitWarning(parameter);
 		warningSts = 1;
 	}
-	else if((value >= max - max*TOLERANCE) && (value <= max)){
+	else if(highLimitCheck){
 		UpdateForHighLimitWarning(parameter);
 		warningSts = 1;
 	}
