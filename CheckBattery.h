b68cf1b17c1b31346df952bbcfe15372eb27277c
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-07-13 00:55:24 +0530

    Delete CheckBattery.h

--- CheckBattery.h
+++
@@ -1,8 +0,0 @@
-#define TEMP_MIN			0
-#define TEMP_MAX			45
-#define SOC_MIN				20
-#define SOC_MAX				80
-#define CHARGERATE_LIMIT	0.8
-
-#define BATTERY_PARAM_NOTINRANGE(param, min, max)		((param < min) || (param > max))
-#define CHARGERATE_NOTINRANGE(param, limit)				  (param > limit)
